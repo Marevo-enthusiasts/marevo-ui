dc4d8725bd20bd596544d9a6e26b4bc5446e2117
Ihor Voznyi <devphase01@gmail.com>
2023-09-18 16:30:30 +0300

    resolve absolute paths issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      '@widgets': '/src/widgets',
-      '@shared': '/src/shared'
-    }
+    alias: [
+      { find: '~', replacement: path.resolve(__dirname, 'src') }
+    ]
   }
 });
