81b1881db150f797c7b1a52efb274cdc7c201452
WLDLHN <vladslav.m@gmail.com>
2023-09-19 18:15:31 +0300

    some modifies with header and hero sections

--- src/widgets/header/Header.tailwind.ts
+++ src/widgets/header/Header.tailwind.ts
@@ -3,12 +3,15 @@ import ctl from '@netlify/classnames-template-literals';
 export const containerStyle = ctl(`
   h-[60px] 
   opacity-90 
-  py-4 
-  px-6 
-  flex 
-  justify-between 
-  items-center
   fixed
   inset-0
   z-[100]
+`);
+
+export const containerElements = ctl(`
+  px-4
+  py-2
+  flex 
+  justify-between 
+  items-center
 `);
--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,12 +1,15 @@
 import { containerStyle } from './Header.tailwind';
+import { containerElements } from './Header.tailwind';
 import { Logotype, Cart, BurgerMenu } from './components';
 
 const Header = () => {
   return (
-    <div className={`${containerStyle}`}>
-      <BurgerMenu />
-      <Logotype />
-      <Cart />
+    <div className={`${containerStyle} after:absolute after:bg-white after:w-full after:h-[1.4px] after:opacity-[.5]`}>
+      <div className={`${containerElements}`}>
+        <BurgerMenu />
+        <Logotype />
+        <Cart />
+      </div>
     </div>
   );
 };
